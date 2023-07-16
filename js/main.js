6c3fc4476b929f5cd686e8d8b80b19da68f770f2
MohamedHendy <mohamadhendy14@gmail.com>
2023-07-16 03:40:18 +0300

    init dashboard

---
+++ js/main.js
@@ -0,0 +1,21 @@
+$(function () {
+  $(document).on('click','.delete-image-dia', function() {
+      $('.image-pic').addClass('d-none')
+      $('.upload-btn-dia').removeClass('d-none')
+  })
+    
+});
+function dragNdrop(event) {
+    var fileName = URL.createObjectURL(event.target.files[0]);
+    var preview = document.getElementById("preview");
+    var previewImg = document.createElement("img");
+    previewImg.setAttribute("src", fileName);
+    preview.innerHTML = "";
+    preview.appendChild(previewImg);
+}
+function drag() {
+    document.getElementById('uploadFile').parentNode.className = 'draging dragBox';
+}
+function drop() {
+    document.getElementById('uploadFile').parentNode.className = 'dragBox';
+}
